eb5f1d3d8378ba8d030640419e517cf22b9d6718
mina5alaf <36233029+mina5alaf@users.noreply.github.com>
2020-08-11 12:38:22 +0200

    updated after review

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
+* [About the Author](#author)
+* [Project Description](#Description)
+* [Technology used](#technology)
 
 ## Instructions
 
@@ -11,3 +14,17 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## About the Author
+
+My name is Mina Khalaf,i am a student at faculty of computers and articial intelligence Cairo univesrsity 
+contact me : +201289655922
+
+## Project Description
+
+adding interactivity to landing page
+dynamic navigation bar builder and setting active section with animation 
+
+## Technology used
+
+javascript ES6
--- js/app.js
+++ js/app.js
@@ -32,10 +32,11 @@ const numofcontainers = document.querySelectorAll(".landing__container");
  *
  */
 //--------------------------------------------------------
-function buildnavbar(){
+function buildnavbar() {
   for (let i = 0; i < numofcontainers.length; i++) {
     let templist = document.createElement("li");
     templist.setAttribute("class", "menu__link");
+    templist.setAttribute("id", "clicked".concat((i + 1).toString()));
     let temppar = document.createElement("span");
     temppar.innerHTML = `<p id="s${i + 1}"> Section ${i + 1}</p>`;
     templist.appendChild(temppar);
@@ -50,14 +51,35 @@ function scrollnavbarhhandler() {
   for (let i = 0; i < numoflist.length; i++) {
     let templi = document.querySelector("#s".concat((i + 1).toString()));
     templi.addEventListener("click", function () {
-      numofcontainers[i].scrollIntoView();
+      numofcontainers[i].scrollIntoView({ behavior: "smooth" });
     });
     activeclasshandler(i);
+    activenavbaritemhandler(i);
   }
+}
+//-------------------------------
+function activenavbaritemhandler(i) {
+  let templi = document.querySelector("#clicked".concat((i + 1).toString()));
+  const numoflist = document.querySelectorAll("span");
+  numoflist[i].addEventListener("click", function () {
+    document
+      .querySelectorAll("li")
+      .forEach((b) => b.removeAttribute("id"));
+    templi.setAttribute('id',"clicked");
+  });
+}
+//-------------------------------
+//i tried to remove the background when i scroll down or up in the page
+// but when i call this function inside onscroll method
+//the whole onscroll method dont work  
+function removeactivenavbarwhenscroll(i){
+  let list = document.querySelectorAll("li");
+  list.removeAttribute("id");
+  list[i].setAttribute("id","clicked");
 }
 
 //-------------------------------
-function activeclasshandler(i){
+function activeclasshandler(i) {
   let templi = document.querySelector("#s".concat((i + 1).toString()));
   templi.addEventListener("click", function () {
     document
@@ -74,17 +96,19 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  const tempsection = document.querySelectorAll('section');
+  const tempsection = document.querySelectorAll("section");
   const navbarsize = navbarheader.getBoundingClientRect();
   for (let i = 0; i < tempsection.length; i++) {
     let rect = tempsection[i].getBoundingClientRect();
-    if (document.body.scrollTop + (1.5*navbarsize.height)>= rect.y  ) {
+    if (document.body.scrollTop + 1.5 * navbarsize.height >= rect.y) {
       document
         .querySelectorAll("section")
         .forEach((b) => b.removeAttribute("class"));
       document
         .querySelector("#section".concat((i + 1).toString()))
         .setAttribute("class", "your-active-class");
+                //call the funtion 
+      //removeactivenavbarwhenscroll(i);
     }
   }
 }
@@ -103,6 +127,7 @@ buildnavbar();
 
 // Scroll to anchor ID using scrollTO event
 scrollnavbarhhandler();
+ 
 /**
  * End Main Functions
  * Begin Events
